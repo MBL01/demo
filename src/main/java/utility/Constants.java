96e75944c6023492d4722ae27ad8e8d6564dc969
Manikanta Birre <manikanta.birre1@ibm.com>
2025-02-17 12:32:51 +0530

    demo

---
+++ src/main/java/utility/Constants.java
@@ -0,0 +1,18 @@
+package utility;
+
+public class Constants {
+
+	
+		// TODO Auto-generated method stub
+		
+		public static final String URL = "\"http://127.0.0.1:4723";
+		public static final String UserName = "";
+		public static final String Password = "";
+		public static String Language ="//android.widget.ImageView[@content-desc=\\\"English\\";
+		public static String deny ="//android.widget.Button[@resource-id=\"com.android.permissioncontroller:id/permission_deny_button\"]";
+		
+		
+
+	
+
+}
